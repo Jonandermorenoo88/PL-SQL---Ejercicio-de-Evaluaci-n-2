df73210926f2a9c0898fed7be36aded47a8a98cb
jonandermorenoo88 <jaincera97@gmail.com>
2024-04-09 11:39:14 +0200

    verifico si el recorrido es existente o no

--- crearViaje.sql
+++ crearViaje.sql
@@ -4,7 +4,12 @@ create or replace procedure crearViaje(
     m_fecha date, 
     m_conductor varchar
     ) is
-    
-begin
+    v_modelo_autocor int;
 
+begin
+    --verificar si el recorrido existe o no
+    select count(*) into v_modelo_autocor from recorridos where idRecorrido = m_idRecorrido;
+    if v_modelo_autocor = 0 then
+        raise_application_error(-20001, 'recorridos inexistente');
+    end if;
 end;
